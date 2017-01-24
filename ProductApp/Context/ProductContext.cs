3ac24a61a2d724c0b6f3fbdf6e0abcc0c1b18e5d
Marshall <Marshall@DESKTOP-3RKCN07>
2017-01-24 16:23:31 +0700

    Add project files.

---
+++ ProductApp/Context/ProductContext.cs
@@ -0,0 +1,14 @@
+﻿using ProductApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ProductApp.Context
+{
+    public class ProductContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+    }
+}
